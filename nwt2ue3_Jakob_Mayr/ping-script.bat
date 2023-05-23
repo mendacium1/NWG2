b9f23a86044c3f4b35d7bbbd676019eef240e38b
s2210239021 <s2210239021@fhooe.at>
2023-05-23 15:47:36 +0200

    init

---
+++ nwt2ue3_Jakob_Mayr/ping-script.bat
@@ -0,0 +1,221 @@
+:: GR41
+>gr41_1_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.113"
+  ping "185.252.73.113"
+  echo.
+  echo.
+)
+
+>gr41_1_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:112::1"
+  ping "2a0c:2343:0:112::1"
+  echo.
+  echo.
+)
+
+
+>gr41_0_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.122"
+  ping "185.252.73.122"
+  echo.
+  echo.
+)
+
+>gr41_0_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:120::2"
+  ping "2a0c:2343:0:120::2"
+  echo.
+  echo.
+)
+
+:: GR42
+>gr42_1_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.117"
+  ping "185.252.73.117"
+  echo.
+  echo.
+)
+
+>gr42_1_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:116::1"
+  ping "2a0c:2343:0:116::1"
+  echo.
+  echo.
+)
+
+
+>gr42_0_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.126"
+  ping "185.252.73.126"
+  echo.
+  echo.
+)
+
+>gr42_0_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:124::2"
+  ping "2a0c:2343:0:124::2"
+  echo.
+  echo.
+)
+
+
+
+:: GR40
+>gr40_2_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.125"
+  ping "185.252.73.125"
+  echo.
+  echo.
+)
+
+>gr40_2_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:124::1"
+  ping "2a0c:2343:0:124::1"
+  echo.
+  echo.
+)
+
+>gr40_1_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.121"
+  ping "185.252.73.121"
+  echo.
+  echo.
+)
+
+>gr40_1_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:120::1"
+  ping "2a0c:2343:0:120::1"
+  echo.
+  echo.
+)
+
+
+>gr40_0_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.4"
+  ping "185.252.73.4"
+  echo.
+  echo.
+)
+
+>gr40_0_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:120::2"
+  ping "2A0C:2343::4"
+  echo.
+  echo.
+)
+
+:: PC41
+>pc41_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.114"
+  ping "185.252.73.114"
+  echo.
+  echo.
+)
+
+>pc41_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:112::2"
+  ping "2a0c:2343:0:112::2"
+  echo.
+  echo.
+)
+
+
+:: PC42
+>pc42_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.118"
+  ping "185.252.73.118"
+  echo.
+  echo.
+)
+
+>pc42_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2a0c:2343:0:116::2"
+  ping "2a0c:2343:0:116::2"
+  echo.
+  echo.
+)
+
+:: Backbone-Router
+>backbone_1_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.12"
+  ping "185.252.73.12"
+  echo.
+  echo.
+)
+
+>backbone_1_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2A0C:2343::12"
+  ping "2A0C:2343::12"
+  echo.
+  echo.
+)
+
+>backbone_0_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.18"
+  ping "185.252.73.18"
+  echo.
+  echo.
+)
+
+>backbone_0_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2A0C:2343:0:16::2"
+  ping "2A0C:2343:0:16::2"
+  echo.
+  echo.
+)
+
+
+:: PC0
+>pac0_4.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "185.252.73.17"
+  ping "185.252.73.17"
+  echo.
+  echo.
+)
+
+>pc0_6.txt (
+  echo.
+  echo C:\Users\admin_SIN^>ping "2A0C:2343:0:16::1"
+  ping "2A0C:2343:0:16::1"
+  echo.
+  echo.
+)
+
+
+type gr41_1_4.txt gr41_1_6.txt gr41_0_4.txt gr41_0_6.txt
+
+type gr42_1_4.txt gr42_1_6.txt gr42_0_4.txt gr42_0_6.txt
+
+type gr40_2_4.txt gr40_2_6.txt gr40_1_4.txt gr40_1_6.txt gr40_0_4.txt gr40_0_6.txt
+
+type pc41_4.txt pc41_4.txt
+
+type pc42_4.txt pc42_4.txt
+
+type backbone_1_4.txt backbone_1_6.txt backbone_0_4.txt backbone_0_6.txt
+
+echo ende im gelände
+pause
